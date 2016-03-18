bc10245cdae2a3af35bfe886931a0a9c87c866f6
Laura Weiner <lauraweiner@icloud.com>
2016-03-18 16:06:04 -0400

    adjusting xyz from accel to servo

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var servo = servolib.use(tessel.port['A']);
 var servo1 = 1; // We have a servo plugged in at position 1
 var servo2 = 2;
 
+// x [x, y]
+// y 
 
+var x = (xyz[0]/2 + 1)
+var y = (xyz[1]/2 + 1)
 
-servo.on('ready', function () {
+servo.on('ready', function (x,y) {
   var position1 = accelX;  //  Target position of the servo between 0 (min) and 1 (max).
   var position2 = accelY;
 
@@ -18,7 +22,7 @@ servo.on('ready', function () {
       console.log('Position (in range 0-1):', position);
       servo.move(servo1, position);
 
-      position += 0.1;
+      position += x;
       if (position > 1) {
         position = 0; // Reset servo position
       }
@@ -28,9 +32,9 @@ servo.on('ready', function () {
   servo.configure(servo2, 0.05, 0.12, function () {
     setInterval(function () {
       console.log('Position (in range 0-1):', position);
-      servo.move(servo1, position);
+      servo.move(servo2, position);
 
-      position += 0.1;
+      position += y;
       if (position > 1) {
         position = 0; // Reset servo position
       }

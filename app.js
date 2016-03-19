28a5c81b1ef542b85cd5880c1482f5ebc2185130
Rich Song <richasong@gmail.com>
2016-03-19 15:35:15 -0400

    Remove some components, final code base needs to be uploaded

--- app.js
+++ app.js
@@ -14,27 +14,11 @@ servo.on('ready', function () {
   var position2 = accelY;
 
   servo.configure(servo1, 0.05, 0.12, function () {
-    setInterval(function () {
-      console.log('Position (in range 0-1):', position);
-      servo.move(servo1, position);
 
-      position += 0.1;
-      if (position > 1) {
-        position = 0; // Reset servo position
-      }
-    }, 500);
   });
 
   servo.configure(servo2, 0.05, 0.12, function () {
-    setInterval(function () {
-      console.log('Position (in range 0-1):', position);
-      servo.move(servo1, position);
 
-      position += 0.1;
-      if (position > 1) {
-        position = 0; // Reset servo position
-      }
-    }, 500);
   });
 
 
